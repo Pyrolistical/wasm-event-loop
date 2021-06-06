4e3805e956ec36f560ccb48cad2bf26eba850c78
Pyrolistical <pyrogx1133@gmail.com>
2021-06-06 11:35:08 -0700

    1 file modified

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const Wabt = require('wabt');
 async function main() {
   const wabt = await Wabt();
 
-  const module = wabt.parseWat('word-counter.wat', `
+  const module = wabt.parseWat(
+    'word-counter.wat',
+    `
     (module
       (func (export "count") (result i32)
         i32.const 69
       )
     )
-  `);
+  `
+  );
 
-  console.log(module.toText({}))
+  console.log(module.toText({}));
+
+  const {
+    instance: {
+      exports: { count }
+    }
+  } = await WebAssembly.instantiate(module.toBinary({}).buffer);
+  console.log(count());
 }
-main()
+main();
