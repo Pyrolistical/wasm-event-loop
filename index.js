e4d65ba7218551a1792994b5edbda6cd4c704ed3
Pyrolistical <pyrogx1133@gmail.com>
2021-06-07 00:10:33 -0700

    2 files modified

--- index.js
+++ index.js
@@ -7,33 +7,75 @@ async function main() {
     'async-adder.wat',
     `(module
         (import "global" "memory" (memory 1))
-        (import "global" "table" (table 2))
-        (elem (i32.const 0) $sumClosure1 $sumClosure2)
-        (import "closure" "createContext" (func $createContext (param $tableIndex u32) (param $scopeOffset u32) (param $scopeLength u32) (result u32))
-        (import "closure" "deleteContext" (func $deleteContext (param $contextPointer u32))
-        (import "promise" "then" (func $then (param $promisePointer u32) (param $tableIndex u32) (param $contextPointer u32))
-        (import "external" "getX" (func $getX (result u32)))
-        (import "external" "getY" (func $getY (result u32)))
-        (func $malloc (param $bytes u32) (result u32) (local $result u32);; bump allocator
-          u32.load 0
-          local.tee $result
+        (import "global" "table" (table 2 funcref))
+        ;;(elem (i32.const 0) $sumClosure1 $sumClosure2)
+        (import "closure" "createContext" (func $createContext (param $tableIndex i32) (param $scopeOffset i32) (result i32)))
+        (import "closure" "deleteContext" (func $deleteContext (param $contextPointer i32)))
+        (import "promise" "then" (func $then (param $promisePointer i32) (param $contextPointer i32) (result i32)))
+        (import "promise" "resolve" (func $resolve (param $resultPointer i32) (result i32)))
+        (import "external" "getX" (func $getX (result i32)))
+        (import "external" "getY" (func $getY (result i32)))
+        (func $malloc (param $bytes i32) (result i32) (local $result i32);; bump allocator
+          i32.const 0
+          i32.load
+          local.set $result
+          i32.const 0
+          local.get $result
           local.get $bytes
-          u32.add
-          u32.store 0
+          i32.add
+          i32.store
           local.get $result
         )
-        (func (export "sum") (local $xPromisePointer u32) (result u32)
+        (func (export "sum") (result i32) (local $xPromisePointer i32) (local $closurePointer i32)
           call $getX
           local.set $xPromisePointer
-          u32.const 
-          call $then $xPromisePointer
-          i32.add
+          i32.const 0
+          i32.const 0
+          call $createContext
+          local.set $closurePointer
+          local.get $xPromisePointer
+          local.get $closurePointer
+          call $then
         )
-        (func (export "sum") (result i32) (local $xPromise u32) (local $xValue i32) (local $yPromise u32) (local $yValue i32)
-          call $getX
-          local.set $xPromise
-          call $closeLoop
+        (func $sumClosure1 (param $thisClosurePointer i32) (param $xValue i32) (result i32) (local $xPointer i32) (local $yPromisePointer i32) (local $closurePointer i32)
+          local.get $thisClosurePointer
+          call $deleteContext
+          call $getY
+          local.set $yPromisePointer
+          i32.const 1
+          i32.const 4
+          call $malloc
+          local.tee $xPointer
+          call $createContext
+          local.set $closurePointer
+          local.get $xPointer
+          local.get $xValue
+          i32.store
+          local.get $yPromisePointer
+          local.get $closurePointer
+          call $then
+        )
+        (func $sumClosure2 (param $thisClosurePointer i32) (param $yValue i32) (result i32) (local $xValue i32) (local $result i32) (local $resultPointer i32)
+          local.get $thisClosurePointer
+          i32.const 4
           i32.add
+          i32.const 0
+          i32.add
+          i32.load
+          local.set $xValue
+          local.get $thisClosurePointer
+          call $deleteContext
+          local.get $xValue
+          local.get $yValue
+          i32.add
+          local.set $result
+          i32.const 4
+          call $malloc
+          local.tee $resultPointer
+          local.get $result
+          i32.store
+          local.get $resultPointer
+          call $resolve
         )
       )
     `
@@ -45,7 +87,29 @@ async function main() {
     instance: {
       exports: { count }
     }
-  } = await WebAssembly.instantiate(module.toBinary({}).buffer);
-  console.log(count());
+  } = await WebAssembly.instantiate(module.toBinary({}).buffer, {
+    global: {
+      memory: new WebAssembly.Memory({
+        initial: 1
+      }),
+      table: new WebAssembly.Table({
+        initial: 2,
+        element: 'anyfunc'
+      })
+    },
+    closure: {
+      createContext() {},
+      deleteContext() {}
+    },
+    promise: {
+      then() {},
+      resolve() {}
+    },
+    external: {
+      getX() {},
+      getY() {}
+    }
+  });
+  // console.log(count());
 }
 main();
